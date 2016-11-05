029229db537fa4a65e1070087c08df3129cc736c
Benjamin Ramtoula <benjir21@hotmail.fr>
2016-11-05 12:44:03 +0100

    Corrections in step counting

--- src/c/main.c
+++ src/c/main.c
@@ -51,17 +51,17 @@ static uint16_t step_count(short *samples){
   // Count and store number of edges in the signal
   for (int i = 9 ; i < 19; ++i)
   {
-    sign1 = samples[1+i-10]-samples[i-10];
-    sign2 = samples[2+i-10]-samples[1+i-10];
+    sign1 = samples[1+i-9]-samples[i-9];
+    sign2 = samples[2+i-9]-samples[1+i-9];
     if (sign1*sign2 < 0)
     {
-      edges[num_edges] = samples[1+i-10];
+      edges[num_edges] = samples[1+i-9];
       num_edges += 1;
-      if (i == 0){
-        differences[i] = edges[i];
+      if (i == 9){
+        differences[i-9] = edges[i-9];
       }
       else{
-        differences[i] = abs(edges[i] - edges[i-1]);
+        differences[i-9] = abs(edges[i-9] - edges[i-1-9]);
         // Store biggest difference in this data set
         if (differences[i] > max_difference && differences[i] < max_threshold)
           max_difference = differences[i];
@@ -142,7 +142,7 @@ static uint16_t step_count(short *samples){
   return num_steps;
 }
 
-//Function that filters given sample array and modifies it with new filtered values 
+//Function that filters given sample array and modifies it with new filtered values
 static void filter_samples(short *samples) {
   short fr[SIZE_DATA], fi[SIZE_DATA];
   int M, lowerbound; // M is the power of 2 that gives us the number of samples we have. lowerbound is when we start to filter
@@ -174,8 +174,8 @@ static void filter_samples(short *samples) {
   // Do the fft
   HNHN = HNHN+M+1+lowerbound+fr[0]+fi[0]; // To avoid unused variable ; Only for Debugging
   fix_fft(fr,fi,M,0);
-  
-  // Filter depending on the lowerbound 
+
+  // Filter depending on the lowerbound
   for (int i = lowerbound; i < SIZE_DATA ;i++){
     fr[i] = 0;
     fi[i] = 0;
@@ -192,22 +192,22 @@ static void filter_samples(short *samples) {
     samples[i] = abs(fr[i]) + abs(fi[i]);
         //APP_LOG(APP_LOG_LEVEL_DEBUG,"\n%d",samples[i]);
 
-    
+
   }
 }
 
 // Function that compute the number of steps in the raw_data[SIZE_DATA]
-static uint16_t compute_steps(short (*raw_data)[SIZE_DATA]) { 
+static uint16_t compute_steps(short (*raw_data)[SIZE_DATA]) {
   short i, squared_norm[SIZE_DATA]; // store the squared norm of the data
-   
+
   // square norm calculation
     for (i = 0; i < SIZE_DATA ; ++i)
-      squared_norm[i] = raw_data[X][i] * raw_data[X][i] + 
+      squared_norm[i] = raw_data[X][i] * raw_data[X][i] +
                     raw_data[Y][i] * raw_data[Y][i] + raw_data[Z][i] * raw_data[Z][i];
-  
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Starting filtering...\n"); 
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Starting filtering...\n");
   filter_samples(squared_norm);
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Starting step counting...\n"); 
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Starting step counting...\n");
   return step_count(squared_norm);
 }
 
@@ -219,16 +219,16 @@ static void accel_data_handler(AccelData *data, uint32_t num_samples) {
   static short counter = 0;            // used to know when we have all samples ready
   static short raw_data[3][SIZE_DATA]; // store those samples
   static char  results[60];
-  
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "counter = %d\n", counter); 
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "counter = %d\n", counter);
   // Store the data from the accel. into one big array
   for (i = 0; i < num_samples ; ++i)
-  { 
+  {
     raw_data[X][i+num_samples*counter] = data[i].x>>5; // >>4 is equivalent to /16
     raw_data[Y][i+num_samples*counter] = data[i].y>>5; // to avoid overflow with values that are too big
     raw_data[Z][i+num_samples*counter] = data[i].z>>5;
   }
-  
+
   if (counter == MAX_COUNTER) // when all the data is ready, send it
   {
     APP_LOG(APP_LOG_LEVEL_DEBUG, "Sending the data...\n");
@@ -273,13 +273,13 @@ static void init(void) {
     int num_samples = NUM_SAMPLES;
     // Subscribe to accelerometer
     accel_data_service_subscribe(num_samples, accel_data_handler);
-    
+
     // Define accelerometer sampling rate
     accel_service_set_sampling_rate(ACCEL_SAMPLING_25HZ);
-  
+
          // Create main Window element and assign to pointer
          main_window = window_create();
-    Layer *window_layer = window_get_root_layer(main_window);  
+    Layer *window_layer = window_get_root_layer(main_window);
 
                 // Create background Layer
                 background_layer = text_layer_create(GRect( 0, 0, 144, 168));
@@ -290,7 +290,7 @@ static void init(void) {
                 helloWorld_layer = text_layer_create(GRect( 20, 65, 100, 20));
                 // Setup layer Information
                 text_layer_set_background_color(helloWorld_layer, GColorClear);
-                text_layer_set_text_color(helloWorld_layer, GColorWhite);       
+                text_layer_set_text_color(helloWorld_layer, GColorWhite);
                 text_layer_set_font(helloWorld_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
          text_layer_set_text_alignment(helloWorld_layer, GTextAlignmentCenter);
     text_layer_set_text(helloWorld_layer, "My new text");
@@ -298,18 +298,18 @@ static void init(void) {
          // Add layers as childs layers to the Window's root layer
     layer_add_child(window_layer, text_layer_get_layer(background_layer));
           layer_add_child(window_layer, text_layer_get_layer(helloWorld_layer));
-  
+
          // Show the window on the watch, with animated = true
          window_stack_push(main_window, true);
-    
+
     // Add a logging message (for debug)
           APP_LOG(APP_LOG_LEVEL_DEBUG, "End of init'");
 }
 
 // deinit function called when the app is closed
 static void deinit(void) {
-  
-    // Destroy layers and main window 
+
+    // Destroy layers and main window
     text_layer_destroy(background_layer);
           text_layer_destroy(helloWorld_layer);
     window_destroy(main_window);
@@ -317,12 +317,12 @@ static void deinit(void) {
 
 int main(void) {
     init();
-    
+
     show_welcome_window();
     app_timer_register(2000, launch_app, NULL);    // lauches init after 2000ms
 
     //show_steps_window();
-  
+
     app_event_loop();
   deinit();
   return EXIT_SUCCESS;
